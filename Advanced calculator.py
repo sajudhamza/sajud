1e98c74ed733c4b5dd408e1bc720cea60e44a398
sajudhamza <63482086+sajudhamza@users.noreply.github.com>
2020-04-24 03:32:15 -0400

    Add files via upload
    An advanced version of Calculator which calculates on Cumulative inputs.

---
+++ Advanced calculator.py
@@ -0,0 +1,36 @@
+
+
+import re
+
+print("Our Magical Calculator")
+print("Type 'quit' to exit")
+
+previous=0
+run=True
+
+def performMath():
+    global run
+    global previous
+    equation=""
+    if previous==0:
+        equation=input("Enter the Equation:")
+    else:
+        equation=input(str(previous))
+
+
+    if equation=="quit":
+        run=False
+    else:
+        equation=re.sub('[a-zA-Z" ",.:;\']','',equation)
+
+        if previous==0:
+            previous=eval(equation)
+        else :
+            previous=eval(str(previous)+equation)
+
+
+        print("you typed",equation,"=", previous)
+
+while run:
+    performMath()
+
